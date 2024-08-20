09ebe3b062af37741e7cc96e66b4b15e894a920d
Caique <caiquegomesdev@gmail.com>
2024-08-19 22:34:18 -0300

    Exercicio resolvido 02

---
+++ exec_resolvido_02/src/exec_resolvido_02/exec_03.java
@@ -0,0 +1,22 @@
+package exec_resolvido_02;
+import java.util.Scanner;
+
+public class exec_03 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D, dif;
+
+	    A = sc.nextInt();
+	    B = sc.nextInt();
+	    C = sc.nextInt();
+	    D = sc.nextInt();
+
+	    dif = A * B - C * D;
+
+	    System.out.println("DIFERENCA = " + dif);	
+		
+		sc.close();
+	}
+}
---
+++ exec_resolvido_02/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module exec_resolvido_02 {
+}
